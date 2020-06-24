814f3c895b39066767d724ff2a2f3d078193d72e
justinandrade <j.andrade.john@gmail.com>
2020-06-24 02:32:01 -0400

    inspiration added to footer

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div class="w-full bg-gray-200 px-8 py-4 text-xs">
+    <div class="flex justify-between w-full bg-gray-200 px-8 py-4 text-xs">
       <p>JusCoding &copy; 2020 All Rights Reserved </p>
+      <span>Design and layout inspire by Onasanya Babatunde.</span>
     </div>
   );
 };
